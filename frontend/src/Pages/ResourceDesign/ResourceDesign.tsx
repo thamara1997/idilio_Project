a21a674c18ff9ff6fb74a902fb742d1ae1040de5
Darshana Thamara <darshanathamara143@gmail.com>
2023-02-01 10:20:33 +0530

    Filter Search

--- frontend/src/Pages/ResourceDesign/ResourceDesign.tsx
+++ frontend/src/Pages/ResourceDesign/ResourceDesign.tsx
@@ -1,11 +1,36 @@
 import ArtCard from "components/ArtCard/ArtCard";
-import React from "react";
+import React, { useState } from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { routeNames } from "routes/route";
 import { cardDetails } from "data/data";
 
 const ResourceDesign = () => {
+  interface Card {
+    id: any;
+    name: any;
+    price: any;
+    link: any;
+    title: any;
+    Category: any;
+    description: any;
+    reviews: any;
+  }
+
+  const cards: Card[] = cardDetails;
+
+  console.log(cards);
+
+  const [filter1, setFilter1] = useState("");
+  const [filter2, setFilter2] = useState("");
+  const [filter3, setFilter3] = useState("");
+
+  const filteredCards = cards.filter(
+    (card) =>
+      card.title.toLowerCase().includes(filter1.toLowerCase()) ||
+      card.price.toLowerCase().includes(filter2.toLowerCase()) ||
+      card.Category.toLowerCase().includes(filter3.toLowerCase())
+  );
   return (
     <div className="text-center">
       <h6 className="mt-[50px] font-bold">Hola !</h6>
@@ -20,32 +45,42 @@ const ResourceDesign = () => {
           <input
             type="text"
             className="w-[600px] h-[3rem] rounded-xl border-[0.5px] border-[#fec850] bg-transparent p-[10px]"
+            value={filter1}
+            onChange={(e) => setFilter1(e.target.value)}
           />
           <div className="absolute right-2 top-4">
             <BiSearchAlt />
           </div>
         </div>
         <div className="">
-          <select className="w-[300px] select  bg-transparent text-center rounded-xl border-[0.5px] border-[#fec850] font-light h-[30px]">
+          <select
+            className="w-[300px] select  bg-transparent text-center rounded-xl border-[0.5px] border-[#fec850] font-light h-[30px]"
+            value={filter3}
+            onChange={(e) => setFilter3(e.target.value)}
+          >
             <option disabled selected>
               Category
             </option>
-            <option>Album Cover</option>
-            <option>Flyers</option>
-            <option>Company Logo</option>
-            <option>Book Cover</option>
-            <option>Mascot Logo</option>
+            <option>ALBUM COVER</option>
+            <option>FLYERS</option>
+            <option>COMPANY LOGO</option>
+            <option>BOOK COVER</option>
+            <option>MASCOT LOGO</option>
           </select>
         </div>
         <div className="">
-          <select className="w-[300px] select  bg-transparent text-center rounded-xl border-[0.5px] border-[#fec850] font-light h-[30px]">
+          <select
+            className="w-[300px] select  bg-transparent text-center rounded-xl border-[0.5px] border-[#fec850] font-light h-[30px]"
+            value={filter2}
+            onChange={(e) => setFilter2(e.target.value)}
+          >
             <option disabled selected>
               Cost
             </option>
-            <option>10$-20$</option>
-            <option>20$-30$</option>
-            <option>30$-50$</option>
-            <option>50$-100$</option>
+            <option>10</option>
+            <option>20</option>
+            <option>30</option>
+            <option>50</option>
           </select>
         </div>
       </div>
@@ -54,7 +89,7 @@ const ResourceDesign = () => {
 
       <div className="flex justify-center items-center text-center mb-[100px]">
         <div className="mt-[80px] grid grid-cols-1 gap-[60px] mx-auto md:grid-cols-3">
-          {cardDetails.map((t: any, i: number) => (
+          {filteredCards.map((t: any, i: number) => (
             <div id="item1" className="w-full carousel-item" key={i}>
               <Link to={routeNames.RDesignDetails.replace(":id", i.toString())}>
                 <ArtCard name={t.name} price={t.price} />
